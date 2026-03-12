8af99f1f577f0fcac94dea379a21aae59e6af1fc
liujiayii <liujiayii@foxmail.com>
2026-03-12 10:37:30 +0800

    feat: 更新examples

--- examples/remix-style/src/pages/about/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react'
-
-const index: FC = () => {
-  return (
-    <p>about/index.tsx</p>
-  )
-}
-
-export default index
--- examples/remix-style/src/pages/blog.authors.tsx
+++
@@ -1,9 +0,0 @@
-import type { FC } from 'react'
-
-const Component: FC = () => {
-  return (
-    <p>blog.authors.tsx</p>
-  )
-}
-
-export default Component
